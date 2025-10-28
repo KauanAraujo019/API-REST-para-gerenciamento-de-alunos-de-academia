470e58b4c67ff126593b84e8c09fef20c5b3cfdc
Kauan Araujo <araujokauan664@gmail.com>
2025-10-28 18:26:06 -0300

    InvoiceService adicionada

--- src/main/java/com/Project/Register_Gym_Goers/services/InvoiceService.java
+++ src/main/java/com/Project/Register_Gym_Goers/services/InvoiceService.java
@@ -3,9 +3,9 @@ package com.Project.Register_Gym_Goers.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GoerService {
+public class InvoiceService {
 
-
+    
 
 
 }
