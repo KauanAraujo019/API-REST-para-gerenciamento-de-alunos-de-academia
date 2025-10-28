069309ecf55a8af923ccaba08c1d63a49a2e6490
Kauan Araujo <araujokauan664@gmail.com>
2025-10-28 16:10:37 -0300

    iniciando implementação do sistema

---
+++ src/main/java/com/Project/Register_Gym_Goers/RegisterGymGoersApplication.java
@@ -0,0 +1,13 @@
+package com.Project.Register_Gym_Goers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterGymGoersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterGymGoersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Project/Register_Gym_Goers/RegisterGymGoersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Register_Gym_Goers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterGymGoersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
